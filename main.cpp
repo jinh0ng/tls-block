cc7666f628dbaef22cc39a9091ee54f98748e580
jinh0ng <lilyhong511@gmail.com>
2025-06-11 11:07:20 +0900

    debug

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@
 #include "tcphdr.h"
 #include "ip.h"
 
-// 구조체 정의
 #pragma pack(push, 1)
 struct PacketInfo
 {
@@ -25,7 +24,6 @@ struct PacketInfo
 };
 #pragma pack(pop)
 
-// 체크섬용 의사 헤더
 #pragma pack(push, 1)
 struct ChecksumHdr
 {
@@ -51,7 +49,6 @@ void usage()
     printf("sample : tls-block wlan0 naver.com\n");
 }
 
-// 인터페이스 MAC, IP 조회
 int getMyInfo(t_info *info, const char *dev)
 {
     struct ifreq ifr;
@@ -62,7 +59,6 @@ int getMyInfo(t_info *info, const char *dev)
     strncpy(ifr.ifr_name, dev, IFNAMSIZ - 1);
     ifr.ifr_name[IFNAMSIZ - 1] = '\0';
 
-    // MAC
     if (ioctl(fd, SIOCGIFHWADDR, &ifr) == 0)
     {
         info->mac = Mac(reinterpret_cast<uint8_t *>(ifr.ifr_hwaddr.sa_data));
@@ -72,7 +68,6 @@ int getMyInfo(t_info *info, const char *dev)
         close(fd);
         return -1;
     }
-    // IP
     if (ioctl(fd, SIOCGIFADDR, &ifr) == 0)
     {
         uint32_t raw = ntohl(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr.s_addr);
@@ -89,7 +84,6 @@ int getMyInfo(t_info *info, const char *dev)
     return 0;
 }
 
-// 일반 체크섬 계산
 uint16_t CheckSum(uint16_t *buf, int size)
 {
     uint32_t sum = 0;
@@ -105,49 +99,40 @@ uint16_t CheckSum(uint16_t *buf, int size)
     return (uint16_t)(~sum);
 }
 
-// TLS ClientHello의 SNI 추출
 bool parseTlsSni(const uint8_t *data, uint32_t len, std::string &sni)
 {
     if (len < 5)
         return false;
     uint32_t pos = 0;
-    // TLS Record
     uint8_t type = data[pos++];
     if (type != 22)
-        return false; // Handshake
-    pos += 2;         // version
+        return false;
+    pos += 2;
     uint16_t recLen = (data[pos] << 8) | data[pos + 1];
     pos += 2;
     if (recLen + 5 > len)
         return false;
-    // Handshake message
     if (data[pos++] != 1)
-        return false; // ClientHello
-    // handshake length
+        return false;
     uint32_t hsLen = (data[pos] << 16) | (data[pos + 1] << 8) | data[pos + 2];
     pos += 3;
     if (hsLen + 4 > recLen)
         return false;
-    // skip version(2), random(32)
     pos += 2 + 32;
     if (pos + 1 > len)
         return false;
-    // session ID
     uint8_t sidLen = data[pos++];
     pos += sidLen;
     if (pos + 2 > len)
         return false;
-    // cipher suites
     uint16_t csLen = (data[pos] << 8) | data[pos + 1];
     pos += 2 + csLen;
     if (pos + 1 > len)
         return false;
-    // compression
     uint8_t compLen = data[pos++];
     pos += compLen;
     if (pos + 2 > len)
         return false;
-    // extensions length
     uint16_t extLen = (data[pos] << 8) | data[pos + 1];
     pos += 2;
     uint32_t extEnd = pos + extLen;
@@ -157,7 +142,7 @@ bool parseTlsSni(const uint8_t *data, uint32_t len, std::string &sni)
         uint16_t extSize = (data[pos + 2] << 8) | data[pos + 3];
         pos += 4;
         if (extType == 0x0000)
-        { // SNI
+        {
             if (pos + 2 > len)
                 return false;
             uint16_t listLen = (data[pos] << 8) | data[pos + 1];
@@ -201,13 +186,27 @@ int main(int argc, char *argv[])
         fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
         return -1;
     }
+    // BPF filter: only TCP dst port 443
+    struct bpf_program fp;
+    if (pcap_compile(handle, &fp, "tcp dst port 443", 1, PCAP_NETMASK_UNKNOWN) < 0)
+    {
+        fprintf(stderr, "pcap_compile failed: %s\n", pcap_geterr(handle));
+        return -1;
+    }
+    if (pcap_setfilter(handle, &fp) < 0)
+    {
+        fprintf(stderr, "pcap_setfilter failed: %s\n", pcap_geterr(handle));
+        return -1;
+    }
+    pcap_freecode(&fp);
+
     if (getMyInfo(&MyInfo, dev) < 0)
     {
         fprintf(stderr, "failed to get interface info for %s\n", dev);
         pcap_close(handle);
         return -1;
     }
-    // raw socket for backward RST
+
     int rsock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if (rsock < 0)
     {
@@ -235,10 +234,8 @@ int main(int argc, char *argv[])
             break;
 
         PacketInfo *pi = (PacketInfo *)packet;
-        // Ethernet IPv4?
         if (pi->ethHdr_.type() != EthHdr::Ip4)
             continue;
-        // IP TCP?
         uint32_t ihl = pi->ipHdr_.hl() * 4;
         if (pi->ipHdr_.p() != IpHdr::Tcp)
             continue;
@@ -248,6 +245,10 @@ int main(int argc, char *argv[])
         if (dataLen == 0)
             continue;
         const uint8_t *payload = packet + sizeof(EthHdr) + ihl + thl;
+        // debug print
+        printf("[DEBUG] payload %u bytes -> first 5 bytes = %02X %02X %02X %02X %02X\n",
+               dataLen,
+               payload[0], payload[1], payload[2], payload[3], payload[4]);
         std::string sni;
         if (!parseTlsSni(payload, dataLen, sni))
             continue;
@@ -255,85 +256,73 @@ int main(int argc, char *argv[])
             continue;
         printf("Blocking SNI: %s\n", sni.c_str());
 
-        // === Forward RST ===
+        // Forward RST
+        PacketInfo *fpkt = (PacketInfo *)malloc(sizeof(PacketInfo));
+        memcpy(fpkt, packet, sizeof(PacketInfo));
+        fpkt->ethHdr_.smac_ = MyInfo.mac;
+        uint16_t newIpLen = htons(ihl + thl);
+        fpkt->ipHdr_.len_ = newIpLen;
+        fpkt->ipHdr_.sum_ = 0;
+        fpkt->ipHdr_.sum_ = CheckSum((uint16_t *)&fpkt->ipHdr_, sizeof(IpHdr));
+        uint32_t origSeq = pi->tcpHdr_.seq();
+        uint32_t newSeq = origSeq + dataLen;
+        fpkt->tcpHdr_.seq_ = htonl(newSeq);
+        fpkt->tcpHdr_.flags_ = TcpHdr::Rst | TcpHdr::Ack;
+        fpkt->tcpHdr_.off_rsvd_ = (sizeof(TcpHdr) / 4) << 4;
+        fpkt->tcpHdr_.sum_ = 0;
+        ChecksumHdr phdr;
+        memset(&phdr, 0, sizeof(phdr));
+        phdr.srcAddr = (uint32_t)pi->ipHdr_.sip();
+        phdr.dstAddr = (uint32_t)pi->ipHdr_.dip();
+        phdr.proto = pi->ipHdr_.p();
+        phdr.tcpLen = htons(sizeof(TcpHdr));
+        uint32_t csum = 0;
+        csum += CheckSum((uint16_t *)&fpkt->tcpHdr_, sizeof(TcpHdr));
+        csum += CheckSum((uint16_t *)&phdr, sizeof(phdr));
+        csum = (csum & 0xFFFF) + (csum >> 16);
+        fpkt->tcpHdr_.sum_ = (uint16_t)csum;
+        pcap_sendpacket(handle, (const u_char *)fpkt, sizeof(PacketInfo));
+        free(fpkt);
+
+        // Backward RST
+        struct
         {
-            PacketInfo *fp = (PacketInfo *)malloc(sizeof(PacketInfo));
-            memcpy(fp, packet, sizeof(PacketInfo));
-            // MAC
-            fp->ethHdr_.smac_ = MyInfo.mac;
-            // IP len, checksum
-            uint16_t newIpLen = htons(ihl + thl);
-            fp->ipHdr_.len_ = newIpLen;
-            fp->ipHdr_.sum_ = 0;
-            fp->ipHdr_.sum_ = CheckSum((uint16_t *)&fp->ipHdr_, sizeof(IpHdr));
-            // TCP seq, flags, off, checksum
-            uint32_t origSeq = pi->tcpHdr_.seq();
-            uint32_t newSeq = origSeq + dataLen;
-            fp->tcpHdr_.seq_ = htonl(newSeq);
-            fp->tcpHdr_.flags_ = TcpHdr::Rst | TcpHdr::Ack;
-            fp->tcpHdr_.off_rsvd_ = (sizeof(TcpHdr) / 4) << 4;
-            // checksum
-            fp->tcpHdr_.sum_ = 0;
-            ChecksumHdr phdr;
-            memset(&phdr, 0, sizeof(phdr));
-            phdr.srcAddr = (uint32_t)pi->ipHdr_.sip();
-            phdr.dstAddr = (uint32_t)pi->ipHdr_.dip();
-            phdr.proto = pi->ipHdr_.p();
-            phdr.tcpLen = htons(sizeof(TcpHdr));
-            uint32_t csum = 0;
-            csum += CheckSum((uint16_t *)&fp->tcpHdr_, sizeof(TcpHdr));
-            csum += CheckSum((uint16_t *)&phdr, sizeof(phdr));
-            csum = (csum & 0xFFFF) + (csum >> 16);
-            fp->tcpHdr_.sum_ = (uint16_t)csum;
-            // send
-            pcap_sendpacket(handle, (const u_char *)fp, sizeof(PacketInfo));
-            free(fp);
-        }
-        // === Backward RST ===
-        {
-            struct
-            {
-                IpHdr ip;
-                TcpHdr tcp;
-            } bp;
-            // IP
-            memcpy(&bp.ip, &pi->ipHdr_, sizeof(IpHdr));
-            bp.ip.sip_ = pi->ipHdr_.dip_;
-            bp.ip.dip_ = pi->ipHdr_.sip_;
-            bp.ip.len_ = htons(ihl + thl);
-            bp.ip.ttl_ = 64;
-            bp.ip.sum_ = 0;
-            bp.ip.sum_ = CheckSum((uint16_t *)&bp.ip, sizeof(IpHdr));
-            // TCP
-            memcpy(&bp.tcp, &pi->tcpHdr_, sizeof(TcpHdr));
-            bp.tcp.sport_ = pi->tcpHdr_.dport_;
-            bp.tcp.dport_ = pi->tcpHdr_.sport_;
-            bp.tcp.seq_ = pi->tcpHdr_.ack_;
-            uint32_t origSeq = pi->tcpHdr_.seq();
-            uint32_t newAck = origSeq + dataLen;
-            bp.tcp.ack_ = htonl(newAck);
-            bp.tcp.flags_ = TcpHdr::Rst | TcpHdr::Ack;
-            bp.tcp.off_rsvd_ = (sizeof(TcpHdr) / 4) << 4;
-            bp.tcp.sum_ = 0;
-            ChecksumHdr ph;
-            memset(&ph, 0, sizeof(ph));
-            ph.srcAddr = (uint32_t)bp.ip.sip_;
-            ph.dstAddr = (uint32_t)bp.ip.dip_;
-            ph.proto = bp.ip.p();
-            ph.tcpLen = htons(sizeof(TcpHdr));
-            uint32_t c2 = 0;
-            c2 += CheckSum((uint16_t *)&bp.tcp, sizeof(TcpHdr));
-            c2 += CheckSum((uint16_t *)&ph, sizeof(ph));
-            c2 = (c2 & 0xFFFF) + (c2 >> 16);
-            bp.tcp.sum_ = (uint16_t)c2;
-            // send raw
-            struct sockaddr_in sin;
-            memset(&sin, 0, sizeof(sin));
-            sin.sin_family = AF_INET;
-            sin.sin_addr.s_addr = inet_addr(std::string(pi->ipHdr_.sip()).c_str());
-            sendto(rsock, &bp.ip, sizeof(IpHdr) + sizeof(TcpHdr), 0,
-                   (struct sockaddr *)&sin, sizeof(sin));
-        }
+            IpHdr ip;
+            TcpHdr tcp;
+        } bpkt;
+        memcpy(&bpkt.ip, &pi->ipHdr_, sizeof(IpHdr));
+        bpkt.ip.sip_ = pi->ipHdr_.dip_;
+        bpkt.ip.dip_ = pi->ipHdr_.sip_;
+        bpkt.ip.len_ = htons(ihl + thl);
+        bpkt.ip.ttl_ = 64;
+        bpkt.ip.sum_ = 0;
+        bpkt.ip.sum_ = CheckSum((uint16_t *)&bpkt.ip, sizeof(IpHdr));
+        memcpy(&bpkt.tcp, &pi->tcpHdr_, sizeof(TcpHdr));
+        bpkt.tcp.sport_ = pi->tcpHdr_.dport_;
+        bpkt.tcp.dport_ = pi->tcpHdr_.sport_;
+        bpkt.tcp.seq_ = pi->tcpHdr_.ack_;
+        uint32_t newAck = origSeq + dataLen;
+        bpkt.tcp.ack_ = htonl(newAck);
+        bpkt.tcp.flags_ = TcpHdr::Rst | TcpHdr::Ack;
+        bpkt.tcp.off_rsvd_ = (sizeof(TcpHdr) / 4) << 4;
+        bpkt.tcp.sum_ = 0;
+        ChecksumHdr ph2;
+        memset(&ph2, 0, sizeof(ph2));
+        ph2.srcAddr = (uint32_t)bpkt.ip.sip_;
+        ph2.dstAddr = (uint32_t)bpkt.ip.dip_;
+        ph2.proto = bpkt.ip.p();
+        ph2.tcpLen = htons(sizeof(TcpHdr));
+        uint32_t c2 = 0;
+        c2 += CheckSum((uint16_t *)&bpkt.tcp, sizeof(TcpHdr));
+        c2 += CheckSum((uint16_t *)&ph2, sizeof(ph2));
+        c2 = (c2 & 0xFFFF) + (c2 >> 16);
+        bpkt.tcp.sum_ = (uint16_t)c2;
+        struct sockaddr_in sin;
+        memset(&sin, 0, sizeof(sin));
+        sin.sin_family = AF_INET;
+        sin.sin_addr.s_addr = inet_addr(std::string(pi->ipHdr_.sip()).c_str());
+        sendto(rsock, &bpkt.ip, sizeof(IpHdr) + sizeof(TcpHdr), 0,
+               (struct sockaddr *)&sin, sizeof(sin));
     }
     close(rsock);
     pcap_close(handle);
